e210486b5422032842a5e33f0207e5161cadd643
ccbc7 <flexibleelbow@gmail.com>
2023-01-14 11:28:10 +0900

    検索機能を２つに分けました。ルーム予約時にマイナスの人数で登録できてしまっていた所を修正しました。予約時に人数と宿泊数で正しい値段を表示しました。検索画面のレスポンシブデザインを調整しました。

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,7 +8,7 @@ class Room < ApplicationRecord
     with_options on: :create do #ルーム登録時バリデーション
         validates :room_name,  presence: true
         validates :address,  presence: true
-        validates :charge,  presence: true
+        validates :charge,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99999999}
         # validates :avatar,  presence: true #デフォルト画像表示のため
     end
 
